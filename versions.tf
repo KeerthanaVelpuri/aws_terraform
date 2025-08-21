5494409777ef6d6dc5359c6a4792364dab1e7830
KeerthanaVelpuri <132149528+KeerthanaVelpuri@users.noreply.github.com>
2025-08-21 09:51:48 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "keerthana-bucket-for-training210825"
+    key = "keerthana.tfstate"
+    region = "eu-west-3"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"

05ff4f6e0fb509889fb2d7fb02dae8b2c9ed3969
KeerthanaVelpuri <132149528+KeerthanaVelpuri@users.noreply.github.com>
2025-08-21 10:00:42 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "keerthana-bucket-for-training210825"
+    bucket = "keerthana-2025"
     key = "keerthana.tfstate"
     region = "eu-west-3"
     use_lockfile = true
